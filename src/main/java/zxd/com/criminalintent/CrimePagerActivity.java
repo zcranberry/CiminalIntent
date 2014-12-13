986aaeccc00b2c5990d737616855f28d284223c3
zcranberry <zxdpku@126.com>
2014-12-13 22:04:29 +0800

    Pager分支第一步

---
+++ src/main/java/zxd/com/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,41 @@
+package zxd.com.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zxd on 2014/12/13.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int pos) {
+                Crime crime = mCrimes.get(pos);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+}
