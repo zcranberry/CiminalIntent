329b2a06ddac7852cc06391471a798cdf683bda3
zcranberry <zxdpku@126.com>
2014-12-13 15:07:22 +0800

    修复bug

--- src/main/java/zxd/com/criminalintent/CrimeListFragment.java
+++ src/main/java/zxd/com/criminalintent/CrimeListFragment.java
@@ -34,7 +34,7 @@ public class CrimeListFragment extends ListFragment {
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);//这个地方也改过，见163页
         //Log.d(TAG, c.getTitle() + " was clicked");
         Intent i = new Intent(getActivity(), CrimeActivity.class);
-        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getDate());
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
 
